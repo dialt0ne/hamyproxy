7649135cb78b906e3c26ec03d2dff2a25a97b5e1
dialt0ne <atonns@corsis.com>
2014-02-11 17:17:30 +0000

    minor doc updates

--- metadata.rb
+++ metadata.rb
@@ -19,7 +19,7 @@ maintainer_email 'atonns@corsis.com'
 license          'Apache 2.0'
 description      'Installs/Configures hamyproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends          'cron'
 depends          'haproxy'
