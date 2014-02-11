e7c11cf28aa7d5a50359de04dff9dd7721eb6cdc
dialt0ne <anthony@tonns.com>
2014-02-11 15:03:09 -0500

    added splay time to restarts

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,6 +15,7 @@
 
 default['hamyproxy']['basedir'] = "/opt/hamyproxy"
 default['hamyproxy']['servers'] = {}
+default['hamyproxy']['splaytime'] = 30
 normal['haproxy']['enable_default_http'] = false
 normal['haproxy']['enable_ssl'] = false
 normal['haproxy']['defaults_options'] = ["dontlognull", "redispatch"]
